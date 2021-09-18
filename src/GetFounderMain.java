6c32865a578b09661474e2e5894b83fdfc08a1c4
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-09-18 08:51:59 +0530

    Initial commit

---
+++ src/GetFounderMain.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class GetFounderMain {
+    public static void main(String[] args) {
+        GetFounder getFounder = new GetFounder();
+        getFounder.setFounder("Priyanka");
+        System.out.println(getFounder.getFounder());
+    }
+}
